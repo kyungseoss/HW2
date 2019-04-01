05b95061eff5ee002ffb1b6a8b023c13bf403a6b
kyungseo <kyungseo96@naver.com>
2019-04-02 00:04:28 +0900

    두번째 숙제 파일 생성

---
+++ Howtwo.py
@@ -0,0 +1,12 @@
+﻿# import random
+# numbers = random.randrange(101)
+numbers = int(input("숫자를 입력하세요: "))
+if (numbers >= 90):
+    print("A")
+elif (numbers >= 80):
+    print("B")
+elif (numbers >= 70):
+    print("C")
+else:
+    print("F")
+    
